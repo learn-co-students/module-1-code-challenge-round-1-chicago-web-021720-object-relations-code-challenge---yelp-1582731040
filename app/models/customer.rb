799f66f8afd5414f236c94beb2e29dedd345eb78
Ethan Davidson <theejdavidson@gmail.com>
2020-02-26 10:34:26 -0600

    done & done

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,46 @@
 class Customer
   attr_accessor :given_name, :family_name
+  @@all = []
 
   def initialize(given_name, family_name)
     @given_name = given_name
     @family_name  = family_name
+    @@all << self
   end
 
   def full_name
     "#{given_name} #{family_name}"
   end
+
+  def self.all
+    @@all
+  end
+
+  def reviews
+    #helper method
+    #returns array of reviews from customer instance
+    Review.all.select { |review| review.customer == self}
+  end
+
+  def restaurants
+    restaurant_arr = self.reviews.map { |review| review.restaurant }
+    restaurant_arr.uniq
+  end
+
+  def add_review(restaurant, rating)
+    Review.new(self, restaurant, rating)
+  end
+
+  def num_reviews
+    self.reviews.size
+  end
+
+  def self.find_by_name(full_name)
+    self.all.find { |customer| customer.full_name == full_name}
+  end
+
+  def self.find_all_by_given_name(given_name)
+    self.all.select { |customer| customer.given_name == given_name }
+  end
+
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -5,4 +5,18 @@ class Restaurant
     @name = name
   end
 
+  def reviews
+    Review.all.select { |review| review.restaurant == self}
+  end
+
+  def customers
+    customer_arr = self.reviews.map { |review| review.customer}
+    customer_arr.uniq
+  end
+
+  def average_star_rating
+    ratings_arr = self.reviews.map { |review| review.rating.to_f }
+    ratings_arr.sum / ratings_arr.size
+  end
+
 end
