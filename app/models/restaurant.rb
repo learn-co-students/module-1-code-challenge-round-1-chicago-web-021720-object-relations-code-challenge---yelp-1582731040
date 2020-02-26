5bb969bec335ecd316c2259a7594892a7decad40
Sam Escobar <sescobar8714@gmail.com>
2020-02-26 09:02:07 -0800

    message

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,27 @@
 class Restaurant
   attr_reader :name
 
+  @@all = []
+
   def initialize(name)
     @name = name
+    @@all = []
+  end
+
+  def customers_review
+  #Must work on customer class 
+  end 
+
+  def review_avg_rating
+  #Must work on review class
+  end
+
+  def review 
+  
+  end 
+
+  def self 
+    @@all = self
   end
 
 end
