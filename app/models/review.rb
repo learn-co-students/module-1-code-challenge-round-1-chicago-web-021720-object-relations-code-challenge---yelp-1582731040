75ea29a391f826e5035a76799013f95afceb811a
owenabbott <ocabbott@gmail.com>
2020-02-26 11:12:54 -0600

    my solution

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,36 @@
 class Review
+  @@all = []
+
+  attr_reader :customer, :restaurant, :rating
+
+  def initialize(customer, restaurant, rating)
+    @customer = customer 
+    @restaurant = restaurant 
+    @rating = rating
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
   
 end
+
+
+
+
+
+#`Review#customer`
+#  - returns the customer object for that review
+#  - Once a review is created, should not be able to change the customer
+# `Review#restaurant`
+#  - returns the restaurant object for that given review
+#  - Once a review is created, should not be able to change the restaurant
+
+#I think the above is all handled by the attr_reader?
+#I think attr_reader :customer for example should be the same as:
+    #def customer
+    #@customer=customer
+    #end
+#?
--- tools/console.rb
+++ tools/console.rb
@@ -3,9 +3,25 @@ require_relative '../config/environment.rb'
 def reload
   load 'config/environment.rb'
 end
+
+
+me = Customer.new("Owen", "Abbott")
+elmilagro = Restaurant.new("El Milagro")
+miraclereview = Review.new(me, elmilagro, 5)
+
+joe = Customer.new("Joe", "Schmo")
+joereview = Review.new(joe, elmilagro, 1)
+
+#elmilagro.average_star_rating = 3!!!!! It works! Or at least that method works. I haven't tested the others. I wrote it all out in 
+#for loops because I"m used to it and I was prioritizing getting everything to run right. If I had more time I'd refactor. It's 10:58 and I'm out of time.
+
+
+
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
+
+#elmilagro.average_star_rating = 3!!!!! It works! Or at least that method works. I haven't tested the others.
