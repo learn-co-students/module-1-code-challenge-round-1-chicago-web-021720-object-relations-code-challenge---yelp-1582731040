8ddbfd98496a9d9f98166c625ee126c2b3aeb24e
doodleman218 <dcwolff218@gmail.com>
2020-02-26 11:02:26 -0600

    code challenge 1

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,27 @@
 class Customer
   attr_accessor :given_name, :family_name
 
+  @@all = [] #for self.all
+
   def initialize(given_name, family_name)
     @given_name = given_name
     @family_name  = family_name
+    @@all << self #glad i tested, bc almost forgot this!
   end
 
   def full_name
     "#{given_name} #{family_name}"
   end
+
+  def self.all #returns array of all instances
+    @@all
+  end
+
+  def restaurants
+    selection = Review.all select do |restaurants|
+
+
+  end
+
+
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,35 @@
 class Restaurant
+  
   attr_reader :name
+
+# Going to probably need @@all 
 
   def initialize(name)
     @name = name
   end
+
+  def reviews
+     selection = Review.all.select do |review|
+      review.restaurant_in == self
+     end
+     binding.pry
+     selection.collect do |x|
+      x.rating  #getting nil
+     end
+  end
+
+  def customers  #not sure if this is unique
+    selection = Review.all.select do |customers|
+      customers.restaurant_in == self
+    end
+    selection.collect do |x|
+      x.customer_in
+    end
+  end
+
+
+
+
+
 
 end
