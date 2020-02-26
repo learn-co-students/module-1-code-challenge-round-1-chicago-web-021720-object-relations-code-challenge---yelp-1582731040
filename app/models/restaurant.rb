d97fb7fe55bb6b5e56904592ac9ea50d545acbe7
github username <github email address>
2020-02-26 11:01:55 -0600

    yusufcelep

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -5,4 +5,17 @@ class Restaurant
     @name = name
   end
 
+  def review
+    Review.all.select {|review| review.restaurant == self}
+  end
+
+  def customers
+    array = self.review.map {|review| review.customer}
+    array.uniq
+  end
+
+  def average_star_rating
+    self.review.collect {|review| review.rating}
+    #This was the last method I was working on, couldn't finish...
+  end
 end
